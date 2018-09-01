9c9617d4bfec4edace5d67830334ef3606dba74d
Your Name <you@example.com>
2018-09-01 21:16:15 +0200

    Move objects intialization and keyup event listener to the engine

--- js/app.js
+++ js/app.js
@@ -130,28 +130,3 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-window.player = new Player();
-window.allEnemies = [new Enemy(), // Generate 5 enemies
-    new Enemy(),
-    new Enemy(),
-    new Enemy(),
-    new Enemy()
-];
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-});
--- js/engine.js
+++ js/engine.js
@@ -28,6 +28,30 @@ var Engine = (function(global) {
     canvas.height = 606;
     doc.body.appendChild(canvas);
 
+    // Now instantiate your objects.
+    // Place all enemy objects in an array called allEnemies
+    // Place the player object in a variable called player
+    window.player = new Player();
+    window.allEnemies = [new Enemy(), // Generate 5 enemies
+        new Enemy(),
+        new Enemy(),
+        new Enemy(),
+        new Enemy()
+    ];
+
+    // This listens for key presses and sends the keys to your
+    // Player.handleInput() method. You don't need to modify this.
+    document.addEventListener('keyup', function(e) {
+        var allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down'
+        };
+
+        player.handleInput(allowedKeys[e.keyCode]);
+    });
+
     doc.addEventListener('game-won', () => {
         // TODO: Show a meassge and ask to play again
         setTimeout(() => {
